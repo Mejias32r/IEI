b0635736d631d211ab9aa80ae88c37d0057c578a
Antopato <90763997+Antopato@users.noreply.github.com>
2024-11-13 14:19:31 +0100

    prueba

--- IEI_project/wrappers/wrapper_csv/views.py
+++ IEI_project/wrappers/wrapper_csv/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+import csv
+import selenium
 # Create your views here.
+
+def readCSVtoJson():
+    print("Voy a abrir")
+    with open('cv.csv', encoding='utf-8') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            print(row)
+
+
+
