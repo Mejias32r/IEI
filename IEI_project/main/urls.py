744d46a2c9a6e0545d8a96372bacdead0cf9d24b
Mejias32r <mejiasriobopepe@gmail.com>
2025-01-02 10:50:56 +0100

    Creación endpoint para coger todos los monumentos

--- IEI_project/main/urls.py
+++ IEI_project/main/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('ventana-carga/', views.ventana_carga, name="ventana_carga"),
     path('vaciar-almacen-datos/', views.vaciar_almacen_datos, name = "vaciar-almacen-datos"),
-    path('cargar-almacen-datos/', views.cargar_almacen_datos, name = "cagar-almacen-datos")
+    path('cargar-almacen-datos/', views.cargar_almacen_datos, name = "cagar-almacen-datos"),
+    path('get-monumentos/', views.get_monumentos, name = "obtener-monumentos"),
 ]
