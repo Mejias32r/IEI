127ec1a81aa8d25ec8fb2ca2687f8e6c889dbf71
Cesar Gimeno Castellote <cesargicas@gmail.com>
2025-01-15 18:47:29 +0100

    Swagger doc XML

--- micro_servicios/wrapper_xml/wrapper_xml/urls.py
+++ micro_servicios/wrapper_xml/wrapper_xml/urls.py
@@ -17,8 +17,24 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .views import extractor_xml
+from rest_framework import permissions
+from rest_framework.permissions import AllowAny
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API Extractor XML",
+        default_version='v1',
+        description="API to extract data from XML files",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('extractor', extractor_xml, name="extractor")
+    path('extractor', extractor_xml, name="extractor"),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
